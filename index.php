551731e8b3157e64c1a78268e06d77ea34ddbe90
Santosl2 <mfilype2019@gmail.com>
2023-04-03 19:50:29 -0300

    feat: deleção e inserção de postagens

--- index.php
+++ index.php
@@ -1,27 +1,28 @@
 <?php include('views/header.php');
 
 if (isset($_POST['login'])) {
-    
+
     $usuario = mysqli_real_escape_string($conexao, $_POST['email']);
     $senha = mysqli_real_escape_string($conexao, $_POST['senha']);
-    
+
     $query = "SELECT email, id 
                 from usuarios 
                 where email = '{$usuario}' 
                 and senha = '{$senha}'";
-    
+
     $result = mysqli_query($conexao, $query);
     $rows = mysqli_fetch_assoc($result);
     $idUsuario = $rows['id'];
 
     $row = mysqli_num_rows($result);
 
-    if(!isset($usuario)){
-        $_SESSION['usuarioInvalido'] = true;  
+    if (!isset($usuario)) {
+        $_SESSION['usuarioInvalido'] = true;
         header('Location: index.php');
     }
-    if($row == 1) {
-        $_SESSION['usuario'] = $usuario;
+    if ($row == 1) {
+        $_SESSION['usuario'] = $rows['nome'];
+        $_SESSION['nome'] = $usuario;
         $_SESSION['id_usuario'] = $idUsuario;
         header("Location: ./views/home.php");
         exit();
@@ -45,19 +46,19 @@ if (isset($_POST['login'])) {
                         <label for="email">E-mail</label>
                         <input name="email" class=" form-control" id="email" placeholder="Email" type="email">
                     </div>
- 
+
                     <div class="form-group mb-4">
                         <label for="password">Senha</label>
                         <input class="form-control" name="senha" id="password" placeholder="******" type="password">
                     </div>
- 
+
                     <div class="form-group">
                         <button type="submit" class="btn btn-primary btn-block" name="login"> Login </button>
                     </div>
-                    <?php if(isset($_SESSION['usuarioInvalido'])) {?>
-                            <div class="alert alert-danger mt-2" role="alert">
-                                Preencha todos os campos corretamente.
-                            </div>
+                    <?php if (isset($_SESSION['usuarioInvalido'])) { ?>
+                        <div class="alert alert-danger mt-2" role="alert">
+                            Preencha todos os campos corretamente.
+                        </div>
                     <?php } ?>
                 </form>
             </article>
@@ -65,6 +66,6 @@ if (isset($_POST['login'])) {
 </main>
 
 <?php
-    unset($_SESSION['verificaEmail']); 
+unset($_SESSION['verificaEmail']);
 ?>
 <?php include('views/footer.php'); ?>
