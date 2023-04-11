fe1219692309cacf1e0edfa90d120a628570aec1
Lorrayne Rodrigues <lorrayne.rodrigues@animaeducacao.com.br>
2023-04-11 15:28:09 -0300

    Realize o cadastro

--- views/cadastro.php
+++ views/cadastro.php
@@ -1,4 +1,25 @@
-<?php include('./header.php');?>
+<?php 
+include('./header.php');
+
+$result = false;
+if (isset($_POST['cadastro']))  {
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+$sql = "INSERT INTO
+        usuarios (nome, email, senha)
+        VALUES ('$nome','$email','$senha')";
+
+$result = mysqli_query($conexao, $sql);
+if($result) {
+    $mensagem = "Usuário Cadastrado com sucesso!";
+}
+else {
+    $mensagem = "Erro ao cadastrar usuário";
+}
+}
+?>
 
 <main>
     <div class="container">
@@ -24,8 +45,13 @@
                 <label for="password" class="form-label text-white">Confirmar senha</label>
                 <input type="password"class="form-control" id="password" placeholder="*******">
                 </div>
-                <button type="submit" name ="cadastro" class="btn btn-light">Realizar cadastro</button>
+                <button type="submit" name="cadastro" class="btn btn-light">Realizar cadastro</button>
             </form>
+            <?php if($result) { ?>
+                    <div class="alert alert-primary mt-2" role="alert">
+                        <?php echo $mensagem ?>
+                    </div>
+                <?php } ?>
             </div>
             <div class="col-md-6 d-flex align-items-center justify-content-center">
             <img src="/seminario-uniasselvi/assets/img/logo.png" alt="Placeholder Image" class="img-fluid logo-login">
@@ -34,3 +60,5 @@
         </div>
     </div>
 </main>
+
+<?php include('./footer.php'); ?>
